70fd48262fa87124fc53de0df81b0c4fa4be8beb
Reidmen <r.rethmawn@gmail.com>
2023-07-28 13:33:55 +0200

    added new folder structure and config files

--- cardiac_benchmark_toolkitx/fiber_generation.py
+++ cardiac_benchmark_toolkitx/fiber_generation.py
@@ -1,106 +1,41 @@
+import argparse
 from dataclasses import dataclass
 import pathlib
-from typing import Literal
+from typing import Literal, TypeAlias
 
-import argparse
-
-from dolfinx.mesh import Mesh, MeshTags
-
-import numpy as np
-
-from mpi4py import MPI
-from petsc4py import PETSc
-from numpy.typing import NDArray
 from dolfinx import fem, io
+from dolfinx.mesh import Mesh, MeshTags
+from mpi4py import MPI
+import numpy as np
+from numpy.typing import NDArray
+from petsc4py import PETSc
 import ufl
 
-
-@dataclass(frozen=True)
-class MARKERS:
-    ENDOCARDIUM = 1
-    EPICARDIUM = 2
+from cardiac_benchmark_toolkitx.data import DEFAULTS, MARKERS, FiberDirections
 
 
-@dataclass(frozen=True)
-class DEFAULTS:
-    R_SHORT_ENDO = 2.5e-2
-    R_SHORT_EPI = 3.5e-2
-    R_LONG_ENDO = 9.0e-2
-    R_LONG_EPI = 9.7e-2
-    FIBER_ALPHA_ENDO = -60.0
-    FIBER_ALPHA_EPI = +60.0
+# TODO wrap in a class, constants are properties
+
+NDArray64: TypeAlias = NDArray[np.float64]
+
+r_short_endo: float = DEFAULTS.R_SHORT_ENDO
+r_short_epi: float = DEFAULTS.R_SHORT_EPI
+r_long_endo: float = DEFAULTS.R_LONG_ENDO
+r_long_epi: float = DEFAULTS.R_LONG_EPI
+alpha_endo: float = DEFAULTS.FIBER_ALPHA_ENDO
+alpha_epi: float = DEFAULTS.FIBER_ALPHA_EPI
 
 
-@dataclass(frozen=True)
-class FiberDirections:
-    fiber: fem.Function
-    sheet: fem.Function
-    sheet_normal: fem.Function
+def alpha(x: NDArray64) -> NDArray64:
+    return (alpha_endo + (alpha_epi - alpha_endo) * x) * np.pi / 180
 
 
-class FiberExpression(UserExpression):
-    """Computes fiber expression using its analytic description."""
+def r_long(x: NDArray64) -> NDArray64:
+    return r_long_endo + (r_long_epi - r_long_endo) * x
 
-    def __init__(self, td, **kwargs) -> None:
-        super().__init__(**kwargs)
-        self.td = td  # transmural distance
 
-    def eval(self, values, x):
-        r_short_endo = DEFAULTS.R_SHORT_ENDO
-        r_short_epi = DEFAULTS.R_SHORT_EPI
-        r_long_endo = DEFAULTS.R_LONG_ENDO
-        r_long_epi = DEFAULTS.R_LONG_EPI
-        alpha_endo = DEFAULTS.FIBER_ALPHA_ENDO
-        alpha_epi = DEFAULTS.FIBER_ALPHA_EPI
-
-        td_x = self.td(x[0], x[1], x[2])
-
-        # compute r_short and r_long using t
-        r_s = r_short_endo + (r_short_epi - r_short_endo) * td_x
-        r_l = r_long_endo + (r_long_epi - r_long_endo) * td_x
-        # compute u, v and assign to values
-        a = np.sqrt(x[1] * x[1] + x[2] * x[2]) / r_s
-        b = np.array(x[0] / r_l)
-        u = np.arctan2(a, b)
-        v = 0.0 if u < 1e-7 else np.pi - np.arctan2(x[2], -x[1])
-
-        # define local base using derivate matrix
-        # (e_1, e_2, e_3) = D(e_t, e_u, e_v)
-        dr_s = r_short_epi - r_short_endo
-        dr_l = r_long_epi - r_long_endo
-        cos_u, sin_v = np.cos(u), np.sin(v)
-        sin_u, cos_v = np.sin(u), np.cos(v)
-        e_11, e_12, e_13 = dr_l * cos_u, -r_l * sin_u, 0.0
-        e_21, e_22, e_23 = (
-            dr_l * cos_u * sin_v,
-            r_s * cos_u * cos_v,
-            -r_s * sin_u * sin_v,
-        )
-        e_31, e_32, e_33 = (
-            dr_s * sin_u * sin_v,
-            r_s * cos_u * sin_v,
-            r_s * sin_u * cos_v,
-        )
-
-        # e_1 = np.array([e_11, e_21, e_31])
-        e_2 = np.array([e_12, e_22, e_32])
-        e_3 = np.array([e_13, e_23, e_33])
-
-        # normalize columns
-        for vec in [e_2, e_3]:
-            vec /= np.linalg.norm(vec)
-
-        # rotate in alpha (rad) to obtain the fiber direction
-        alpha = (alpha_endo + (alpha_epi - alpha_endo) * td_x) * np.pi / 180
-        fiber = np.sin(alpha) * e_2 + np.cos(alpha) * e_3
-
-        # define fibers using the values r, u, v
-        values[0] = fiber[0]
-        values[1] = fiber[1]
-        values[2] = fiber[2]
-
-    def value_shape(self) -> tuple[Literal[3]]:
-        return (3,)
+def r_short(x: NDArray64) -> NDArray64:
+    return r_short_endo + (r_short_epi - r_short_endo) * x
 
 
 class SheetNormalExpression(UserExpression):
@@ -274,33 +209,99 @@ def transmural_distance_problem(
     return transmural_distance
 
 
-def build_orientations(
+def compute_fiber_direction(
+    distance: fem.Function, fibers_space: fem.VectorFunctionSpace
+) -> fem.Function:
+
+    distance_array = distance.x.array
+    alpha_distance = alpha(distance_array)
+    r_long_distance = r_long(distance_array)
+    r_short_distance = r_short(distance_array)
+
+    # get coordinates
+    x = distance.function_space.dofmap.dof_layout
+
+    # compute u, v and assign to values
+    a = np.sqrt(x[1] * x[1] + x[2] * x[2]) / r_short_distance
+    b = np.array(x[0] / r_long_distance)
+    u = np.arctan2(a, b)
+    v = np.pi - np.arctan2(x[2], -x[1])
+    v[u < 1e-7] = 0
+
+    # define local base using derivate matrix
+    # (e_1, e_2, e_3) = D(e_t, e_u, e_v)
+    dr_s = r_short_epi - r_short_endo
+    dr_l = r_long_epi - r_long_endo
+    cos_u, sin_v = np.cos(u), np.sin(v)
+    sin_u, cos_v = np.sin(u), np.cos(v)
+    e_11, e_12, e_13 = dr_l * cos_u, -r_l * sin_u, 0.0
+    e_21, e_22, e_23 = np.array(
+        [
+            dr_l * cos_u * sin_v,
+            r_s * cos_u * cos_v,
+            -r_s * sin_u * sin_v,
+        ]
+    )
+    e_31, e_32, e_33 = np.array(
+        [
+            dr_s * sin_u * sin_v,
+            r_s * cos_u * sin_v,
+            r_s * sin_u * cos_v,
+        ]
+    )
+
+    # e_1 = np.array([e_11, e_21, e_31])
+    e_2 = np.array([e_12, e_22, e_32])
+    e_3 = np.array([e_13, e_23, e_33])
+
+    # normalize columns
+    for vec in [e_2, e_3]:
+        vec /= np.linalg.norm(vec, axis=0)
+
+    # rotate in alpha (rad) to obtain the fiber direction
+    fiber = np.sin(alpha_distance) * e_2 + np.cos(alpha_distance) * e_3
+
+    # define fibers using the values r, u, v
+    values[0] = fiber[0]
+    values[1] = fiber[1]
+    values[2] = fiber[2]
+
+
+def compute_sheet_normal_direction(
+    distance: fem.Function, fibers_space: fem.VectorFunctionSpace
+) -> fem.Function:
+    pass
+
+
+def compute_sheet_direction(
+    distance: fem.Function, fibers_space: fem.VectorFunctionSpace
+) -> fem.Function:
+    pass
+
+
+def build_directions(
     mesh: Mesh,
     transmural_distance: fem.Function,
     degree: int,
 ) -> FiberDirections:
 
-    fiber_expression = FiberExpression(transmural_distance, degree=degree)
-    sheet_normal_expression = SheetNormalExpression(
-        transmural_distance, degree=degree
+    fibers_space = fem.VectorFunctionSpace(mesh, ("CG", degree))
+
+    fiber_direction = compute_fiber_direction(
+        transmural_distance, fibers_space
     )
-    sheet_expression = SheetExpression(
-        fiber=fiber_expression,
-        sheet_normal=sheet_normal_expression,
-        degree=degree,
+    sheet_normal_direction = compute_sheet_normal_direction(
+        transmural_distance, fibers_space
+    )
+    sheet_direction = compute_sheet_direction(
+        transmural_distance, fibers_space
     )
 
-    fibers_function_space = VectorFunctionSpace(mesh, "CG", degree)
-
-    f0 = fem.Function(fibers_function_space, name="f0")
-    s0 = fem.Function(fibers_function_space, name="s0")
-    n0 = fem.Function(fibers_function_space, name="n0")
-
-    f0.assign(interpolate(fiber_expression, fibers_function_space))
-    s0.assign(interpolate(sheet_expression, fibers_function_space))
-    n0.assign(interpolate(sheet_normal_expression, fibers_function_space))
-
-    fiber_directions = FiberDirections(fiber=f0, sheet=s0, sheet_normal=n0)
+    fiber_directions = FiberDirections(
+        fiber=fiber_direction,
+        sheet=sheet_direction,
+        sheet_normal=sheet_normal_direction,
+    )
     print("built fiber, sheet and sheet_normal directions")
 
     return fiber_directions
@@ -357,7 +358,7 @@ def main(
     deg = int(function_space_degree.lower()[1])
     mesh, mesh_function = read_mesh(path_to_mesh)
     transmural_distance = transmural_distance_problem(mesh, mesh_function, deg)
-    fibers = build_orientations(mesh, transmural_distance, deg)
+    fibers = build_directions(mesh, transmural_distance, deg)
     save_mesh_to_files(mesh, mesh_function, path_to_save)
     save_fibers_to_files(fibers, path_to_save)
 
